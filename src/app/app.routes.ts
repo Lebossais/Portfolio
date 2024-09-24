f0bcd0eff87cb76ba58da821368bc050a36a9f0c
Lebossais <BenjaminDC59@gmail.com>
2024-09-24 16:08:14 +0200

    first commit - 24/09/2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component:AboutComponent},
+  { path: 'projects', component:ProjectsComponent},
+  { path: 'contact', component:ContactComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutes { }
