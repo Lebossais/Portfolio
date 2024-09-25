bdd940adaa70445ac0c14d619a87e85d46eb9f23
Lebossais <BenjaminDC59@gmail.com>
2024-09-25 17:12:23 +0200

    about - test - 25/09/2024

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -8,8 +8,7 @@ import { Component, ElementRef, AfterViewInit, Renderer2  } from '@angular/core'
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
-
+/*
   constructor(private el: ElementRef, private renderer: Renderer2) {
   }
 
@@ -47,4 +46,44 @@ export class AboutComponent {
     // Vérifie dès le chargement
     checkScroll();
   }
+  */
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    const timelineItems: NodeListOf<HTMLElement> = this.el.nativeElement.querySelectorAll('.timeline-item');
+    const progressBars: NodeListOf<HTMLElement> = this.el.nativeElement.querySelectorAll('.progress');
+
+    const fillProgressBars = () => {
+      progressBars.forEach((progress) => {
+        const value = progress.getAttribute('data-value');
+        this.renderer.setStyle(progress, 'width', `${value}%`);
+      });
+    };
+
+    const checkScroll = () => {
+      const triggerBottom = window.innerHeight / 5 * 4;
+
+      timelineItems.forEach((item: HTMLElement) => {
+        const itemTop = item.getBoundingClientRect().top;
+        if (itemTop < triggerBottom) {
+          this.renderer.addClass(item, 'visible'); // Ajoute la classe visible pour l'animation de défilement
+        }
+      });
+
+      progressBars.forEach((progress) => {
+        const progressTop = progress.getBoundingClientRect().top;
+        if (progressTop < triggerBottom) {
+          fillProgressBars();
+        }
+      });
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+    // Lancer l'animation si déjà visible
+    checkScroll();
+  }
+
+
 }
