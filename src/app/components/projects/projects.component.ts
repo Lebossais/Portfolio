9682326c571d0e914d6107ff461d993aa0277d64
Lebossais <BenjaminDC59@gmail.com>
2024-10-02 12:35:04 +0200

    Project - 02/10/2024

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,21 +13,28 @@ import {NgForOf} from "@angular/common";
 export class ProjectsComponent {
   projects =[
     {
-      title: 'Projet 1',
       description: "Voici la description du projet que j'ai pu réaliser",
-      image: '/assets/project1.jpg',
+      image: '/assets/Projet2.jpg',
       link: 'https://lienversleprojet1.com'
     },
     {
-      title: 'Projet 2',
       description: "Voici la description du projet que j'ai pu réaliser",
-      image: '/assets/project2.jpg',
+      image: '/assets/Projet3.jpg',
       link: 'https://lienversleprojet1.com'
     },
     {
-      title: 'Projet 3',
       description: "Voici la description du projet que j'ai pu réaliser",
-      image: '/assets/project3.jpg',
+      image: '/assets/Projet1.jpg',
+      link: 'https://lienversleprojet1.com'
+    },
+    {
+      description: "Voici la description du projet que j'ai pu réaliser",
+      image: '/assets/Projet4.jpg',
+      link: 'https://lienversleprojet1.com'
+    },
+    {
+      description: "Voici la description du projet que j'ai pu réaliser",
+      image: '/assets/Projet3.jpg',
       link: 'https://lienversleprojet1.com'
     },
   ]
