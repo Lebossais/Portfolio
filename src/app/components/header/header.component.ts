aecc1abb671ca8e36297a7b66e07b0d5a847c01a
Lebossais <BenjaminDC59@gmail.com>
2024-10-02 08:39:42 +0200

    header + compétences - 02/10/2024

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  previousScrollPosition = window.scrollY;
+  isHeaderVisible = true;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollPosition = window.scrollY;
+
+    if(currentScrollPosition > this.previousScrollPosition && currentScrollPosition > 100) {
+      this.isHeaderVisible = false;
+    } else {
+      this.isHeaderVisible = true;
+    }
+    this.previousScrollPosition = currentScrollPosition;
+  }
 
 }
